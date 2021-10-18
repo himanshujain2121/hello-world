512d5f419ae27d54b6daae81d13e2088af7da3d4
Himanshu <himanshujain2121@gmail.com>
2021-10-18 22:27:26 +0530

    SSR with dynamic parameter

---
+++ pages/news/[category].js
@@ -0,0 +1,37 @@
+function ArticleByCategory({articles,category}) {
+    console.log(articles)
+    return (
+        <>
+            <h1>showing news for category {category}</h1>
+            {
+                
+                articles.map(article => {
+                    return (
+                        <div key={article.id}>
+                        <h2>{article.id} {article.title}</h2>
+                        <p>{article.description}</p>
+                        <hr></hr>
+                    </div>
+                    )
+                    
+                })
+            }
+        </>
+    )
+}
+
+export default ArticleByCategory
+
+export async function getServerSideProps(context) {
+    const { params } = context
+    const { category } = params
+    const response = await fetch(`http://localhost:4000/news?category=${category}`)
+    const data = await response.json();
+    //console.log(data)
+    return {
+        props: {
+            articles: data,
+            category,
+        }
+    }
+}
