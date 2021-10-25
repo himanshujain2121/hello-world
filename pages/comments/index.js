1b4dbec0226d8070cda3751408129168eba20916
Himanshu <himanshujain2121@gmail.com>
2021-10-25 20:17:47 +0530

    add post api

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -3,6 +3,7 @@ import { useState } from "react"
 function CommentsPage() {
 
     const [comments, setComments] = useState([''])
+    const [comment, setComment] = useState('')
 
     const fetchComments = async () => {
         const response = await fetch('/api/comments')
@@ -10,8 +11,26 @@ function CommentsPage() {
         setComments(data)
     }
 
+    const submitComment = async () => {
+        const response = await fetch('api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ comment }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        console.log(data)
+    }
+
     return (
         <>
+            <input
+                type='text'
+                value={comment}
+                onChange={e => setComment(e.target.value)}>
+            </input>
+            <button onClick={submitComment}>Submit Comments</button>
             <button onClick={fetchComments}>Load Comments</button>
             {
                 comments.map(comment => {
